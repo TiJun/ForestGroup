7c508f0534507819726db61667d03e887db9f7b2
TiJun <9tomek7@gmail.com>
2023-08-13 19:43:33 +0200

    created aboutus section on all media queries

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,13 @@
 const burgerBtn = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.mobile-nav-list')
+const listItems = document.querySelectorAll('.nav-mobile-list-item')
 const handleNav = () => {
     burgerBtn.classList.toggle('is-active')
     navMobile.classList.toggle('mobile-active')
 }
+listItems.forEach(el => el.addEventListener('click', () => {
+    burgerBtn.classList.remove('is-active')
+    navMobile.classList.remove('mobile-active')
+}))
 burgerBtn.addEventListener('click', handleNav)
 feather.replace()
